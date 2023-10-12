a2b245849380a2fdbed7822366142bd0ec2f948f
Junior Oliva <jrfoliva@gmail.com>
2023-10-11 22:50:26 -0300

    Add project files.

---
+++ PrintService.cs
@@ -0,0 +1,45 @@
+﻿namespace GenericsCSharp
+{
+    internal class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is Full!");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty!");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            string aux = "[";
+            for (int i = 0; i < _count; i++)
+            {
+                aux += _values[i].ToString();
+                if (i < _count - 1)
+                {
+                    aux += ", ";
+                }
+                else
+                {
+                    aux += "]";
+                }
+            }
+            Console.WriteLine(aux);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GenericsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many values?: ");
+            int qtd = Convert.ToInt32(Console.ReadLine());
+
+            PrintService<int> ps = new PrintService<int>(); //set to int, generic class
+            for(int i = 1; i <= qtd; i++)
+            {
+                Console.Write($"{i}#: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                ps.AddValue(value);
+            }
+            ps.Print();
+            Console.WriteLine("First: "+ps.First());
+        }
+    }
+}
